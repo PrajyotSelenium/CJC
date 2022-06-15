caa672f25d1849a27de99494efea547f38cc19db
PrajyotSelenium <90840446+PrajyotSelenium@users.noreply.github.com>
2022-06-16 02:43:08 +1030

    Update Github.java

--- Github_practice/src/com/practice/Github.java
+++ Github_practice/src/com/practice/Github.java
@@ -4,7 +4,15 @@ public class Github {
         public void m1()
 	{
 	    System.out.println("m1 method");
-	}		
+	}
+	 public void m2()
+	{
+	    System.out.println("m2 method");
+	}	
+	 public void m3()
+	{
+	    System.out.println("m3 method");
+	}	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Main method");
